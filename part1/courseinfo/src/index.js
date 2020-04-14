c549283bf06319ace3ca0248dcf4ff239fde43c2
EseAlli <essealli.ea@gmail.com>
2020-04-14 19:03:25 +0100

    exercises updated

--- part1/courseinfo/src/index.js
+++ part1/courseinfo/src/index.js
@@ -33,25 +33,6 @@ const Total = ({ parts }) => {
   return <p>Number of exercises {exercises_total}</p>;
 };
 
-const App = () => {
-  const course = {
-    name: "Half Stack application development",
-    parts: [
-      {
-        name: "Fundamentals of React",
-        exercises: 10,
-      },
-      {
-        name: "Using props to pass data",
-        exercises: 7,
-      },
-      {
-        name: "State of a component",
-        exercises: 14,
-      },
-    ],
-  };
-
   return (
     <>
       <Header course={course.name} />
