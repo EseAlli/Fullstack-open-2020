0ebe19c9ae29b07203dad9cd02a9ff7934791e19
EseAlli <essealli.ea@gmail.com>
2021-12-23 20:12:05 +0100

    exercises 4.13 - 4.14

--- part4/bloglist/controllers/blogs.js
+++ part4/bloglist/controllers/blogs.js
@@ -1,10 +1,9 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
 
-blogsRouter.get('/', (request, response) => {
-  Blog.find({}).then(blogs => {
-    response.json(blogs)
-  })
+blogsRouter.get('/', async(request, response) => {
+  const blogs = await Blog.find({})
+  return response.status(200).json(blogs)
 })
 
 blogsRouter.post('/', async (request, response, next) => {
@@ -26,6 +25,27 @@ blogsRouter.post('/', async (request, response, next) => {
   }
 })
 
+blogsRouter.delete( '/:id', async(request, response) =>{
+  const {id} = request.params
+  const deletedBlog = await Blog.findByIdAndDelete(id)
+  if (deletedBlog){
+    return response.status(204)
+  }
+  else{
+    return response.status(400).send("Could not Delete")
+  }
+})
 
+blogsRouter.put('/:id', async(request, response)=>{
+  const {id} = request.params
+  const body = request.body
+
+  const blog = {
+    likes: body.likes
+  }
+
+  const updatedBlog = await Blog.findByIdAndUpdate(id, blog, {new: true})
+  return response.status(200).json(updatedBlog)
+})
 
 module.exports = blogsRouter
